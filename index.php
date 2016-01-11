ffda08f190754586f6b8bb663ec9e1c62e90b7e0
Andy Feng <anndyfeng1@gmail.com>
2016-01-11 08:51:12 -0800

    v0.2
    redirect to ec2

--- index.php
+++ index.php
@@ -5,8 +5,15 @@
         $_SESSION['power'] = 0;
         $_SESSION['ham1'] = 0;
         $_SESSION['ham2'] = 0;
+        $_SESSION['ham3'] = 0;
         $_SESSION['ham-total'] = 0;
         $_SESSION['log'] = array();
+
+
+        $_SESSION['price1'] = 1500;
+        $_SESSION['price2'] = 10000;
+        $_SESSION['price3'] = 50000;
+
     }
     else {
         $power = (int)$_SESSION['power'];
@@ -14,7 +21,11 @@
 //        var_dump($_SESSION['power']);
 //        var_dump($power);
         $_SESSION['gold_total'] = $_SESSION['gold_total'] + $power;
-        $_SESSION['ham-total'] = $_SESSION['ham1'] + $_SESSION['ham2'];
+        $_SESSION['ham-total'] = $_SESSION['ham1'] + $_SESSION['ham2'] + $_SESSION['ham3'];
+
+
+
+
     }
 ?>
 <!DOCTYPE html>
@@ -30,28 +41,36 @@
             var numPowerConverter = Math.pow(10, intDecimalPlaces);
             return ~~(numToTruncate * numPowerConverter)/numPowerConverter;
         }
-//  ************************************************
+//  ***************************************************************************
 
 //  ************************ COMMAS FUNCTION ***********************************************
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!|d))/g, ",");
 }
-//  ***********************************************
+//  **************************************************************************
         var power1 = 0 ;
         var power = 0 ;
+        var sec = 0;
         var ham1 = 0;
         var ham11 =0;
         var ham2 = 0;
         var ham22 =0;
+        var ham3 = 0;
+        var ham33 =0;
         <?php if ($_SESSION['ham-total'] > 0){ ?>
             window.setInterval(
                 function (){
                     ham11 = <?= $_SESSION['ham1']?>;
                     ham1 = ham11;
                     ham22 = <?= $_SESSION['ham2']?>;
-                    ham2 = ham22 * 10;
-                    power1 = power + ham1 + ham2;
+                    ham2 = ham22 * 5;
+                    ham33 = <?= $_SESSION['ham3']?>;
+                    ham3 = ham33 * 40;
+
+                    power1 = power + ham1 + ham2 + ham3;
                     power = truncator(power1, 0);
+
+
 
 
 
@@ -59,6 +78,7 @@ function numberWithCommas(x) {
                     document.getElementById("power-num1").value = power ;
                     document.getElementById("power-num0").value = power ;
                     document.getElementById("ham2p").value = power ;
+                    document.getElementById("ham3p").value = power ;
                     document.getElementById("farm-p").value = power ;
                     document.getElementById("casino-p").value = power ;
                 }, 100);
@@ -94,8 +114,8 @@ function numberWithCommas(x) {
                             <button class="dig-button" type="submit">Dig for gold</button>
                         </form>
                         <div class="dig-info">
-                            <h2>FARM</h2>
-                            <p>earns 10-20 Gold</p>
+                            <h2>CAVE</h2>
+                            <p>earns 1-200 Gold</p>
                         </div>
                     </div>
                     <div class="dig2">
@@ -115,7 +135,7 @@ function numberWithCommas(x) {
                     <div class="buy-info">
                         <h3 class="ham1">Basic Ham</h3>
                         <img src="images/ham1.gif">
-                        <p class="black">cost: 30 gold</p>
+                        <p class="black">cost: <?=$_SESSION['price1']?> gold</p>
                         <p class="blue">gold / second: 10</p>
                     </div>
                     <form class="buy-form" action="process.php" method="post">
@@ -126,10 +146,10 @@ function numberWithCommas(x) {
                 </div>
                 <div class="buy2">
                     <div class="buy-info">
-                        <h3 class="ham2">cheerleader Ham</h3>
+                        <h3 class="ham2">cheer Ham</h3>
                         <img src="images/ham2.gif">
-                        <p class="black">cost: 1000 gold</p>
-                        <p class="blue">gold / second: 100</p>
+                        <p class="black">cost: <?=$_SESSION['price2']?> gold</p>
+                        <p class="blue">gold / second: 50</p>
                     </div>
                     <form class="buy-form" action="process.php" method="post">
                         <input type="hidden" id="ham2p" name='value' value="">
@@ -137,7 +157,26 @@ function numberWithCommas(x) {
                         <button class="buy-button" type="submit">BUY</button>
                     </form>
                 </div>
+                <div class="buy2">
+                    <div class="buy-info">
+                        <h3 class="ham3">spin Ham</h3>
+                        <img src="http://rs5.pbsrc.com/albums/y179/brianmichaelwendt/Funny/hamsterdance.gif~c200">
+                        <p class="black">cost: <?=$_SESSION['price3']?> gold</p>
+                        <p class="blue">gold / second: 400</p>
+                    </div>
+                    <form class="buy-form" action="process.php" method="post">
+                        <input type="hidden" id="ham3p" name='value' value="">
+                        <input type='hidden' name='action' value='ham3'>
+                        <button class="buy-button" type="submit">BUY</button>
+                    </form>
+                </div>
+
+
+
+
+
             </div>
+
             <div class="reset">
                 <form class="form-reset" action="process.php" method="post">
                     <input type='hidden' name='action' value='reset'>
@@ -156,11 +195,15 @@ function numberWithCommas(x) {
             </div>
             <div id="show-wrapper">
                 <div class="show-header">
-
                     <div class="dig-description">
-                        <p>Get gold. Buy hamster. Win.</p>
+                        <p>Stats</p>
                     </div>
-
+                    <div class="stats-wrapper1">
+                        <p>total hamsters:<span><?= $_SESSION['ham-total'] ?></span></p>
+                    </div>
+                    <div class="stats-wrapper1">
+                        <p> total gold/sec:<span><?= $_SESSION['ham1'] * 10 + $_SESSION['ham2'] * 100 + $_SESSION['ham3'] * 700?></span></p>
+                    </div>
                 </div>
                 <div class="show-ham">
                     <?php
@@ -176,9 +219,19 @@ function numberWithCommas(x) {
                     <?php
                     if ($_SESSION['ham2'] > 0){
                         $num2 = $_SESSION['ham2'];
-                        echo "<p><span class='ham2'>Cheerleader Ham</span> x $num2 </p>";
+                        echo "<p><span class='ham2'>Cheer Ham</span> x $num2 </p>";
                         for ($i=0; $i < $_SESSION['ham2']; $i++) {
                             echo "<div id='ham2'></div>";
+                        }
+                    }?>
+                </div>
+                <div class="show-ham">
+                    <?php
+                    if ($_SESSION['ham3'] > 0){
+                        $num3 = $_SESSION['ham3'];
+                        echo "<p><span class='ham3'>Spin Ham</span> x $num3 </p>";
+                        for ($i=0; $i < $_SESSION['ham3']; $i++) {
+                            echo "<div id='ham3'></div>";
                         }
                     }?>
                 </div>
--- process.php
+++ process.php
@@ -3,6 +3,7 @@
     date_default_timezone_set('America/Los_Angeles');
 	$time_stamp = date('F jS Y g:i:s a');
     $collect = 0;
+    // farm and casino *****************************************************
 	if(isset($_POST['action']) && $_POST['action'] == 'farm'){
         // collect
         if ($_POST['value'] != 0) {
@@ -10,10 +11,10 @@
             $collect = $_POST['value'];
             array_unshift($_SESSION['log'], "<h4>Collected $collect gold from hamsters!!!&nbsp&nbsp&nbsp($time_stamp)</h4>");
         }
-		$gold = rand(10,20);
+		$gold = rand(1,200);
 		$_SESSION['gold_total'] = $_SESSION['gold_total'] + $gold;
 		// $message = "got "
-		array_unshift($_SESSION['log'], "<h6>You entered a farm and earned $gold gold.&nbsp&nbsp&nbsp($time_stamp)</h6>");
+		array_unshift($_SESSION['log'], "<h6>You entered a cave and earned $gold gold.&nbsp&nbsp&nbsp($time_stamp)</h6>");
 	}
 	if(isset($_POST['action']) && $_POST['action'] == 'casino'){
         // collect
@@ -35,16 +36,18 @@
 	}
     // buy hamsters *****************************************************
     if(isset($_POST['action']) && $_POST['action'] == 'ham1'){
-        if($_SESSION['gold_total'] > 30) {
+        if($_SESSION['gold_total'] > $_SESSION['price1']) {
             // collect
             if ($_POST['value'] != 0) {
                 $_SESSION['power'] = $_POST['value'];
                 $collect = $_POST['value'];
                 array_unshift($_SESSION['log'], "<h4>Collected $collect gold from hamsters!!!&nbsp&nbsp&nbsp($time_stamp)</h4>");
             }
-            $_SESSION['gold_total'] = $_SESSION['gold_total'] - 30;
-            array_unshift($_SESSION['log'], "<h5>You bought 1 basic hamster for 30 gold.&nbsp&nbsp&nbsp($time_stamp)</h5>");
+            $_SESSION['gold_total'] = $_SESSION['gold_total'] - $_SESSION['price1'];
+            $price = $_SESSION['price1'];
+            array_unshift($_SESSION['log'], "<h5>You bought 1 basic ham for $price gold.&nbsp&nbsp&nbsp($time_stamp)</h5>");
             $_SESSION['ham1'] = $_SESSION['ham1'] + 1;
+            $_SESSION['price1'] = floor($_SESSION['price1'] * 1.15);
         }
         else {
             // collect
@@ -57,16 +60,18 @@
         }
     }
     if(isset($_POST['action']) && $_POST['action'] == 'ham2'){
-        if($_SESSION['gold_total'] > 1000) {
+        if($_SESSION['gold_total'] > $_SESSION['price2']) {
             // collect
             if ($_POST['value']!= 0) {
                 $_SESSION['power'] = $_POST['value'];
                 $collect = $_POST['value'];
                 array_unshift($_SESSION['log'], "<h4>Collected $collect gold from hamsters!!!&nbsp&nbsp&nbsp($time_stamp)</h4>");
             }
-            $_SESSION['gold_total'] = $_SESSION['gold_total'] - 1000;
-            array_unshift($_SESSION['log'], "<h5>You bought 1 cheerleader hamster for 1000 gold.&nbsp&nbsp&nbsp($time_stamp)</h5>");
+            $_SESSION['gold_total'] = $_SESSION['gold_total'] - $_SESSION['price2'];
+            $price = $_SESSION['price2'];
+            array_unshift($_SESSION['log'], "<h5>You bought 1 cheer ham for $price gold.&nbsp&nbsp&nbsp($time_stamp)</h5>");
             $_SESSION['ham2'] = $_SESSION['ham2'] + 1;
+            $_SESSION['price2'] = floor($_SESSION['price2'] * 1.15);
         }
         else {
             // collect
@@ -78,6 +83,35 @@
             array_unshift($_SESSION['log'], "<p>You do not have enough gold to buy that!!&nbsp&nbsp&nbsp($time_stamp)</p>");
         }
     }
+    if(isset($_POST['action']) && $_POST['action'] == 'ham3'){
+        if($_SESSION['gold_total'] > $_SESSION['price3']) {
+            // collect
+            if ($_POST['value']!= 0) {
+                $_SESSION['power'] = $_POST['value'];
+                $collect = $_POST['value'];
+                array_unshift($_SESSION['log'], "<h4>Collected $collect gold from hamsters!!!&nbsp&nbsp&nbsp($time_stamp)</h4>");
+            }
+            $_SESSION['gold_total'] = $_SESSION['gold_total'] - $_SESSION['price3'];
+            $price = $_SESSION['price3'];
+            array_unshift($_SESSION['log'], "<h5>You bought 1 spin ham for $price gold.&nbsp&nbsp&nbsp($time_stamp)</h5>");
+            $_SESSION['ham3'] = $_SESSION['ham3'] + 1;
+            $_SESSION['price3'] = floor($_SESSION['price3'] * 1.15);
+        }
+        else {
+            // collect
+            if ($_POST['value']) {
+                $_SESSION['power'] = $_POST['value'];
+                $collect = $_POST['value'];
+                array_unshift($_SESSION['log'], "<h4>Collected $collect gold from hamsters!!!&nbsp&nbsp&nbsp($time_stamp)</h4>");
+            }
+            array_unshift($_SESSION['log'], "<p>You do not have enough gold to buy that!!&nbsp&nbsp&nbsp($time_stamp)</p>");
+        }
+    }
+
+
+
+
+
     if(isset($_POST['action']) && $_POST['action'] == 'get-gold'){
         if ($_POST['value']!= 0) {
             $_SESSION['power'] = $_POST['value'];
